eba23baa02562c3d99cbc2d6b4670384b295171d
nvohra <nvohra@paypal.com>
2015-02-06 09:56:50 +0530

    Initial Commit

---
+++ olaappathon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  olaappathon
+//
+//  Created by Vohra, Nikant on 06/02/15.
+//  Copyright (c) 2015 Vohra, Nikant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ olaappathon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  olaappathon
+//
+//  Created by Vohra, Nikant on 06/02/15.
+//  Copyright (c) 2015 Vohra, Nikant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
